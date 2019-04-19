c0382ac087d2c53ecc45170664461d82e388a7a5
Laurence Taylor <ltaylorapp@gmail.com>
2019-04-19 10:36:22 +0000

    Refactored directory12.rb

--- Exercises/directory12.rb
+++ Exercises/directory12.rb
@@ -1,20 +1,17 @@
+@students = [] # an empty array available to all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
-  students
 end
 
 def print_header
@@ -32,9 +29,33 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-if students.count > 0
-  print_header
-  print(students)
-  print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
 end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    selection = gets.chomp
+    case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
